88218c4b4e90ebc196651ae35852b91b6c3296b6
Niket Shah <shahniket643@gmail.com>
2024-03-20 20:33:48 +0530

    Completed the Item Details Page

--- src/components/home/Item.jsx
+++ src/components/home/Item.jsx
@@ -40,7 +40,7 @@ const Item = ({ item }) => {
                             </div>
                         </div>
                     </div>
-                    <Link to="/item-details">
+                    <Link to={`/item-details/${item.nftId}`}>
                         <img
                             src={item.nftImage}
                             className="lazy nft__item_preview"
@@ -49,7 +49,7 @@ const Item = ({ item }) => {
                     </Link>
                 </div>
                 <div className="nft__item_info">
-                    <Link to="/item-details">
+                    <Link to={`/item-details/${item.nftId}`}>
                         <h4>{item.title}</h4>
                     </Link>
                     <div className="nft__item_price">{item.price} ETH</div>
